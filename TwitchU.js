abf112058f2192bf85d9abfb6f71d59ed11fb2ed
lordgankoo <lordgankoo@gmail.com>
2015-12-11 15:39:55 -0800

    Tests

--- TwitchU.js
+++ TwitchU.js
@@ -1,6 +1,11 @@
 $(function () {
+    var initStatus;
     Twitch.init({ clientId: 'g8b3gmetao07foo9k9u2y7tlku7z10o' }, function (error, status) {
         // the sdk is now loaded
+        initStatus = status;
+        if (status.authenticated) {
+            $('.twitch-connect').hide();
+        }
     });
 
     $('.twitch-connect').click(function () {
@@ -9,18 +14,20 @@ $(function () {
         });
     });
 
-    Twitch.api({ method: 'streams/followed', params: { stream_type: 'live' } }, function (error, list) {
-        for (var i = 0; i < list._total; i++) {
-            //Generate the preview and link
-            var preview = $("<img></img>").attr("src", list.streams[i].preview.medium);
-            var link = $("<a></a>");
-            link.text(list.streams[i].channel.display_name);
-            link.attr("href", list.streams[i].channel.url + "/popout");
+    if (initStatus.authenticated) {
+        Twitch.api({ method: 'streams/followed', params: { stream_type: 'live' } }, function (error, list) {
+            for (var i = 0; i < list._total; i++) {
+                //Generate the preview and link
+                var preview = $("<img></img>").attr("src", list.streams[i].preview.medium);
+                var link = $("<a></a>");
+                link.text(list.streams[i].channel.display_name);
+                link.attr("href", list.streams[i].channel.url + "/popout");
 
-            //Put the preview and link into a <p> and append to <body>
-            var p = $("<p></p>");
-            p.append(preview, "<br>", link, "<br>playing " + list.streams[i].channel.game);
-            $("body").append(p);
-        }
-    });
+                //Put the preview and link into a <p> and append to <body>
+                var p = $("<p></p>");
+                p.append(preview, "<br>", link, "<br>playing " + list.streams[i].channel.game);
+                $("body").append(p);
+            }
+        });
+    }
 });
