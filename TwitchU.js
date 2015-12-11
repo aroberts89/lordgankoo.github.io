c84db16118fc9e49001a59be1af5dd5f9e4b4004
lordgankoo <lordgankoo@gmail.com>
2015-12-11 13:26:04 -0800

    Initial commit

---
+++ TwitchU.js
@@ -0,0 +1,11 @@
+$(function () {
+    Twitch.init({ clientId: 'g8b3gmetao07foo9k9u2y7tlku7z10o' }, function (error, status) {
+        // the sdk is now loaded
+    });
+
+    $('.twitch-connect').click(function () {
+        Twitch.login({
+            scope: ['user_read', 'channel_read']
+        });
+    });
+});
