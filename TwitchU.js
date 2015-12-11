4f81d7e27ce6677a635d77a61ad27eb69c43816f
lordgankoo <lordgankoo@gmail.com>
2015-12-11 14:35:54 -0800

    Tests

--- TwitchU.js
+++ TwitchU.js
@@ -11,7 +11,7 @@ $(function () {
 
     Twitch.api({ method: 'streams/followed', params: { stream_type: 'live' } }, function (error, list) {
         for (var i = 0; i < list._total; i++) {
-            var linkText = $("<p></p>").text("list.streams[i].channel.url");
+            var linkText = $("<p></p>").text(list.streams[i].channel.url);
             $("body").append(linkText);
             console.log(list.streams[i].channel.url);
         }
